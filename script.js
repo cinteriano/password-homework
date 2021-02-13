16462aaf8a7bf14e5c86a6fbd365be09b1fde335
chris interiano <cinteriano@live.com>
2021-02-13 15:50:18 -0800

    added variables and empty arrays' ' README.md index.html script.js style.css:

---
+++ script.js
@@ -0,0 +1,25 @@
+// Assignment Code
+var generateBtn = document.querySelector("#generate");
+var numbers = []
+var lowercase = []
+var uppercase = []
+
+
+
+
+
+
+
+
+
+// Write password to the #password input
+function writePassword() {
+  var password = generatePassword();
+  var passwordText = document.querySelector("#password");
+
+  passwordText.value = password;
+
+}
+
+// Add event listener to generate button
+generateBtn.addEventListener("click", writePassword);
