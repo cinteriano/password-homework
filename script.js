a0ea82e774b11b75f7e81b6e09dc3177b7466de1
chris interiano <cinteriano@live.com>
2021-02-14 17:34:40 -0800

    completed homework

--- script.js
+++ script.js
@@ -130,16 +130,6 @@ function writePassword() {
 
 
 
-// Write password to the #password input
-function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
-
-  passwordText.value = password;
-
-}
-
-// Add event listener to generate button
 
 
 
