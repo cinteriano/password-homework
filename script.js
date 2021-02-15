7f0994f93c8302472fffead26015f4d2d141c421
chris interiano <cinteriano@live.com>
2021-02-14 17:27:39 -0800

    making finishing touches

--- script.js
+++ script.js
@@ -1,9 +1,10 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
-var numbers = ["1", "2", "3", "4" , "5" , "6" , "7" , "8" , "9" , "0"]
-var lowerCase = ["a" , "b" , "c" , "d" , "e" , "f" , "g" , "h" , "i" , "j" , "k" , "l" , "m" , "n" , "o" , "p" , "q" , "r" , "s" , "t" , "u" , "v" , "w" , "x" , "y" , "z"]
-var upperCase = ["A" , "B" , "C" , "D" ,  "E" , "F" , "G" , "H" , "I" , "J" , "K" , "L" , "M" , "N" , "O" , "P" , "Q" , "R" , "S" , "T" , "U" , "V" , "W" , "X" , "Y" , "Z"]
-var specialCharacters = ["!" , "@" , "#" , "$" , "%", "&" , "+" , " * "]
+generateBtn.addEventListener("click" , writePassword);
+var numbers = ["1", "2", "3", "4" , "5" , "6" , "7" , "8" , "9" , "0"];
+var lowerCase = ["a" , "b" , "c" , "d" , "e" , "f" , "g" , "h" , "i" , "j" , "k" , "l" , "m" , "n" , "o" , "p" , "q" , "r" , "s" , "t" , "u" , "v" , "w" , "x" , "y" , "z"];
+var upperCase = ["A" , "B" , "C" , "D" ,  "E" , "F" , "G" , "H" , "I" , "J" , "K" , "L" , "M" , "N" , "O" , "P" , "Q" , "R" , "S" , "T" , "U" , "V" , "W" , "X" , "Y" , "Z"];
+var specialCharacters = ["!" , "@" , "#" , "$" , "%", "&" , "+" , " * "];
 
 
 
@@ -89,6 +90,13 @@ for(var i = 0; i < confirmLength; i++) {
 
 }
 
+function writePassword() {
+  var password = generatePassword();
+  var passwordText = document.querySelector("#password");
+
+  passwordText.value = password;
+
+}
 
 
 
@@ -132,7 +140,7 @@ function writePassword() {
 }
 
 // Add event listener to generate button
-generateBtn.addEventListener("click" , writePassword)
+
 
 
 
